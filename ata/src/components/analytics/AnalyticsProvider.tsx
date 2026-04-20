288b4659a9b5d1e66dcfacdf7a799eabd4cfb722
loicjerry47-domainex <loicjerry47@gmail.com>
2026-04-20 00:16:30 -0400

    clean up

--- ata/src/components/analytics/AnalyticsProvider.tsx
+++
@@ -1,5 +0,0 @@
-// This file has been consolidated into AnalyticsTracker.tsx
-// All analytics functionality is now provided by the AnalyticsProvider component
-// exported from AnalyticsTracker.tsx
-
-export { AnalyticsProvider, useAnalytics, useRealTimeAnalytics, useErrorTracking } from './AnalyticsTracker';
